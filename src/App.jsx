d2a7c332d6f0a3554066c787a621b208d1c3db2e
stevecavallin <steve.cavallin@gmail.com>
2026-05-23 23:23:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -425,9 +425,9 @@ export default function App(){
         if(!deviceId){deviceId=uid();localStorage.setItem('fcm_device_id',deviceId);}
 
         const token=await getToken(messaging,{vapidKey});
-        if (token) {
-          // Sovrascrive tutti i token precedenti con solo quello corrente
-          wait db.set(`fcm_tokens/${user}/${deviceId}`, token);
+        if(token){
+          // Salva token sotto l'utente corrente in Firebase
+          await db.set(`fcm_tokens/${user}/${deviceId}`,token);
         }
       }catch(e){
   alert('Errore notifiche: ' + e.message);
